065a9bd5c5b65552951089ecefd3cabe2123b6e8
SHD08 <163332347+SHD08@users.noreply.github.com>
2024-05-23 23:23:36 +0300

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,39 @@
+# TODO опишите здесь функции для решения задачи
+
+
+#if __name__ == '__main__':
+    # TODO запустите здесь все необходимые функции
+
+def enternumber(enter_str):
+    norm = True
+    while norm:
+        try:
+            num = float(input(enter_str))
+            norm = False
+        except:
+            print('Введите число заново')
+    return num
+
+
+def operation1():
+    while True:
+        oper = input('Выберите операцию: ')
+        if oper == '+':
+            return first + second
+        elif oper =='-':
+            return first - second
+        elif oper == '*':
+            return first * second
+        elif oper == '/':
+            try:
+                return first / second
+            except:
+                print('деление на ноль запрещено')
+            return 0
+        else:
+            print('Некорректная операция')
+
+print('Привет!')
+first = enternumber('Введите первое число: ') #float(input('Введите число: '))
+second = enternumber('Введите второе число: ') #float(input('Введите число: '))
+print('Результат: ', operation1())
